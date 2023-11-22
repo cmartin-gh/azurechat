7edc50d888d6e1331d8f9d46337e520edea56567
Chris Martin <10935229+cmartin-gh@users.noreply.github.com>
2023-11-22 05:22:40 +0000

    Initial commit

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["https://zany-carnival-v6vrgwj9vwrf6xpp-3000.app.github.dev", "localhost:3000"]
+    } 
+  }
 };
 
 module.exports = nextConfig;
